c3192a379ab6bb0710f673befb28a4000e72e68d
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-08-15 15:39:46 +0900

    Title: 합차 공식, Time: 294ms, Memory: 19MB, Status: Passed - CodeTree

--- 240815/합차 공식/sum-formula.js
+++ 240815/합차 공식/sum-formula.js
@@ -4,12 +4,26 @@ const num = Number(fs.readFileSync('/dev/stdin', 'utf8').trim());
 
 let answer = [];
 
-for (let i = 1; i <= num; i++){
-    for (let j = 1; j <= i; j++) {
-        if (i*i - j*j === num){
-            answer.push(i);
+// a = x - n, b = x + n, ab = num
+// num의 약수여야함
+// x = a+b / 2 이므로 a+b, a-b는 짝수
+
+for (let a = 1; a <= num; a++ ) {
+    if (num % a === 0) {
+        let b = num / a;
+        
+        let x = (a + b) / 2;
+        let n = (a - b) / 2;
+
+        if (x > 0 && n > 0 && Number.isInteger(x) && Number.isInteger(n)) {
+            answer.push(x)
         }
     }
 }
 
-console.log(answer.join('\n'))
+if (answer.length === 0) {
+    console.log(-1);
+} else {
+    answer.sort((a, b) => a - b);
+    console.log(answer.join('\n'));
+}
