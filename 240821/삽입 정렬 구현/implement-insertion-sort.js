6e74f816c8458c388c26cb4b60d17d81eda85a8f
Ahyeon, Jung <75254185+a-honey@users.noreply.github.com>
2024-08-21 20:55:41 +0900

    Title: 삽입 정렬 구현, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240821/삽입 정렬 구현/implement-insertion-sort.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const [num, input] = fs.readFileSync('/dev/stdin', 'utf8').trim().split('\n');
+
+const array = input.split(' ');
+
+for (let i = 1; i < num; i++) {
+    let j = i - 1;
+    let target = array[i];
+
+    while (j >= 0 && array[j] > target) {
+        array[j+1] = array[j];
+        j--;
+        array[j+1] = target;
+    }
+}
+
+console.log(array.join(' '))
